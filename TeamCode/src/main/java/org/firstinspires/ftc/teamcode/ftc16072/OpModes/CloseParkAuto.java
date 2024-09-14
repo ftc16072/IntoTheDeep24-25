076b18d9f619d04f510d6840c9edca6254a1e66b
mahin <mahinwaghray@gmail.com>
2024-09-14 17:33:41 -0400

    Code for parking ready, fix trajectories

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/CloseParkAuto.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class CloseParkAuto extends QQOpMode{
+    private double step = 0;
+    public void init(){
+        super.init();
+        robot.otos.setOtosPosition(13.5,-61.5,0);
+    }
+    public void loop(){
+        if(step == 0){
+            boolean doneDriving = nav.driveToPositionIN(13.5,-58.5,0);
+            if(doneDriving){
+                step = 1;
+            }
+        }else if(step == 1){
+            boolean doneDriving = nav.driveToPositionIN(69,-60,0);
+            if(doneDriving){
+                step = 2;
+            }if(step == 2){
+                telemetry.addData("parked", 0);
+            }
+        }
+    }
+}
