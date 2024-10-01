f06c64fad58f9ce61200d1bfa4f6ffae5fad6d0e
mahin <mahinwaghray@gmail.com>
2024-09-30 20:37:57 -0400

    Fixed order of telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/QQOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/QQOpMode.java
@@ -9,13 +9,14 @@ import org.firstinspires.ftc.teamcode.ftc16072.Util.Navigation;
 
 public abstract class QQOpMode extends OpMode {
     public Robot robot = new Robot();
-    Navigation nav = new Navigation(robot, telemetry);
+    Navigation nav;
 
     @Override
     public void init() {
-        robot.init(hardwareMap);
-        robot.controlHub.resetGyro();
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        nav = new Navigation(robot, telemetry);
+
+        robot.init(hardwareMap);
         robot.controlHub.resetGyro();
     }
 
