deb568f5181e6f05274844942c1a892d6651ce59
joshua <joshua@randomsmiths.com>
2024-10-26 18:13:44 -0400

    added code for wrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Claw.java
@@ -7,33 +7,45 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestServo;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 @Config
 public class Claw extends QQMechanism {
     public static double CLAW_CLOSE_POSITION = 0.3;
     public static double CLAW_OPEN_POSITION = 0;
+    public static double WRIST_START_POSITION = 0;
+    public static double WRIST_END_POSITION = 0.67;
+
 
 
     Servo clawServo;
+    Servo wristServo;
+
 
     @Override
     public void init(HardwareMap hwMap) {
         clawServo = hwMap.get(Servo.class, "claw_servo");
+        wristServo = hwMap.get(Servo.class, "wrist_servo");
     }
+
 
     @Override
     public List<QQTest> getTests() {
-        return Collections.singletonList(
-                new TestServo("claw_movement", CLAW_OPEN_POSITION, CLAW_CLOSE_POSITION, clawServo)
+        return Arrays.asList(
+                new TestServo("claw_movement", CLAW_OPEN_POSITION, CLAW_CLOSE_POSITION, clawServo),
+                new TestServo("wrist_movement", WRIST_START_POSITION, WRIST_END_POSITION, wristServo)
         );
+
     }
 
-    public void open() {
-        clawServo.setPosition(CLAW_OPEN_POSITION);
+
+    public void wristStart() {wristServo.setPosition(WRIST_START_POSITION);
+    }
+    public void wristEnd() {wristServo.setPosition(WRIST_END_POSITION);
     }
 
-    public void close() {
-        clawServo.setPosition(CLAW_CLOSE_POSITION);}}
+    public void open() {clawServo.setPosition(CLAW_OPEN_POSITION);
+    }
 
-
+    public void close() {clawServo.setPosition(CLAW_CLOSE_POSITION);}
+    }
