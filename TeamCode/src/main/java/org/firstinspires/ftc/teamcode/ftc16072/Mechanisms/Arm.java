b44e19c694963c8151ce5f36f2eb6334910c92a2
joshua <joshua@randomsmiths.com>
2024-11-03 20:06:54 -0500

    added no turn teleop for scoring under

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Arm.java
@@ -27,8 +27,10 @@ public class Arm extends QQMechanism{
     public int desiredPos;
     public double motorPower;
     public static int PLACEMENT_POSITION = 1030;
+    public static int UNDER_PLACEMENT_POSITION = 940;
     public static int INTAKE_POSITION = 315;
     public static int PLACING_POSITION = 1300;
+    public static int UNDER_PLACING_POSITION = 640;
     public static int AUTO_DRIVE_POSITION = 120;
     public static int GROUND_POSITION = -10;
     static final int WRIST_THRESHOLD = 500;
@@ -57,7 +59,11 @@ public class Arm extends QQMechanism{
     public void goToPlacement(){
         desiredPos = PLACEMENT_POSITION;
     }
+    public void goToUnderPlacement(){
+        desiredPos = UNDER_PLACEMENT_POSITION;
+    }
     public void place(){desiredPos = PLACING_POSITION;}
+    public void placeUnder(){desiredPos = UNDER_PLACING_POSITION;}
     public void goToDrive(){desiredPos = AUTO_DRIVE_POSITION;}
     public void goToGround(){desiredPos = GROUND_POSITION;}
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/OpModes/NoTurnTeleop.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode.ftc16072.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ftc16072.Mechanisms.MecanumDrive;
+
+@TeleOp
+public class NoTurnTeleop extends QQOpMode{
+
+    public static final double TRIGGER_THRESHOLD = 0.5;
+    public static final int MANUAL_CHANGE = 5;
+    private boolean isPlacing = false;
+
+    public void init(){
+        isPlacing = false;
+        super.init();
+        robot.arm.telemetry = telemetry;
+    }
+    public void loop(){
+        super.loop();
+        double forward = -gamepad1.left_stick_y;
+        double left = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
+
+        if (gamepad1.left_trigger > TRIGGER_THRESHOLD ){
+            robot.mecanumDrive.setSpeed(MecanumDrive.Speed.TURBO);
+        } else if (gamepad1.left_bumper) {
+            robot.mecanumDrive.setSpeed(MecanumDrive.Speed.SLOW);
+        } else robot.mecanumDrive.setSpeed(MecanumDrive.Speed.NORMAL);
+
+        if (gamepad1.a){
+            robot.arm.goToIntake();
+            robot.claw.open();
+        }else if (gamepad1.b) {
+            robot.claw.close();
+            robot.arm.goToUnderPlacement();
+        }else if (gamepad1.dpad_right){
+            robot.claw.wristStart();
+        }else if (gamepad1.dpad_left) {
+            robot.claw.wristEnd();
+        }else if (gamepad1.dpad_up){
+            robot.arm.manualPositionChange(MANUAL_CHANGE);
+        }else if (gamepad1.dpad_down){
+            robot.arm.manualPositionChange(-MANUAL_CHANGE);
+        }
+        if (gamepad1.y && gamepad1.dpad_right){
+            robot.controlHub.resetGyro();
+        }
+        if (gamepad1.right_trigger > TRIGGER_THRESHOLD) {
+            robot.claw.close();
+        }else if (gamepad1.right_bumper){
+        robot.claw.open();}
+
+
+        if(gamepad1.x){
+            robot.arm.placeUnder();
+            isPlacing = true;
+        }else if(!gamepad1.x && isPlacing){
+            robot.claw.open();
+            robot.arm.goToIntake();
+
+
+
+
+
+
+
+
+
+            isPlacing = false;
+        }else{
+            nav.driveFieldRelative(forward, left, rotate);
+        }
+    }
+}
