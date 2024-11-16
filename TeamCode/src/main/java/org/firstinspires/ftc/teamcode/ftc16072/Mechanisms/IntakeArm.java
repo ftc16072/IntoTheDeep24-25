b088fd869bb9f30dc9fc0c6737a3332ccde22131
Joshua Smith <joshua@randomsmiths.com>
2024-11-16 14:41:29 -0500

    use goToPos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/IntakeArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/IntakeArm.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestTwoServos;
@@ -26,26 +25,21 @@ public class IntakeArm extends QQMechanism {
         rightArmServo = hwMap.get(Servo.class, "right_arm_servo");
         rightArmServo.setDirection(Servo.Direction.REVERSE);
     }
-    public void goToPos(double position){
+    protected void goToPos(double position){
         leftArmServo.setPosition(position);
         rightArmServo.setPosition(position);
-
     }
     public void goToDropPos(){
-        leftArmServo.setPosition(ARM_DROP_POSITION);
-        rightArmServo.setPosition(ARM_DROP_POSITION);
+        goToPos(ARM_DROP_POSITION);
     }
     public void goToIntake(){
-        leftArmServo.setPosition(ARM_INTAKE_POSITION);
-        rightArmServo.setPosition(ARM_INTAKE_POSITION);
+        goToPos(ARM_INTAKE_POSITION);
     }
     public void searching(){
-        leftArmServo.setPosition(SEARCHING_POSITION);
-        rightArmServo.setPosition(SEARCHING_POSITION);
+        goToPos(SEARCHING_POSITION);
     }
     public void transfer(){
-        leftArmServo.setPosition(TRANSFER_POSITION);
-        rightArmServo.setPosition(TRANSFER_POSITION);
+        goToPos(TRANSFER_POSITION);
     }
 
 
