f9f0271cedf24ded9b50ce1e7333c23dc5620f3d
diya mishra <msdiyamishra@gmail.com>
2025-01-20 22:32:09 -0500

    vision portal testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/IntakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/IntakeClaw.java
@@ -1,14 +1,23 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 
+import android.util.Size;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestServo;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestTwoServos;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestWebcam;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.RotatedRect;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,10 +35,18 @@ public class IntakeClaw extends QQMechanism {
     Servo clawServo;
     Servo leftWristServo;
     Servo rightWristServo;
+    WebcamName webcamName;
     double CLOSED_TIME = 0.5;
     double OPEN_TIME = 0.5;
     double leftWristServoPos;
     double rightWristServoPos;
+
+    public static final int CAMERA_WIDTH = 640;
+    public static final int CAMERA_HEIGHT = 480;
+    int CENTER_X = CAMERA_WIDTH / 2;
+    int CENTER_Y = CAMERA_HEIGHT / 2;
+
+    ColorBlobLocatorProcessor colorLocator;
 
 
     @Override
@@ -37,14 +54,50 @@ public class IntakeClaw extends QQMechanism {
         clawServo = hwMap.get(Servo.class, "intake_claw_servo");
         leftWristServo = hwMap.get(Servo.class, "left_intake_wrist");
         rightWristServo = hwMap.get(Servo.class,"right_intake_wrist");
+        webcamName = hwMap.get(WebcamName.class, "Webcam 1");
         leftWristServo.setDirection(Servo.Direction.REVERSE);
+
+        colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.RED)         // use a predefined color match
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-1, 1, 1, -1))  // search central 1/4 of camera view
+                .setDrawContours(true)                        // Show contours on the Stream Preview
+                .setBlurSize(5)                               // Smooth the transitions between different colors in image
+                .build();
+        @SuppressWarnings("unused")
+        VisionPortal portal = new VisionPortal.Builder()
+                .addProcessor(colorLocator)
+                .setCameraResolution(new Size(CAMERA_WIDTH, CAMERA_HEIGHT))
+                .setCamera(webcamName)
+                .build();
+    }
+
+
+    public ColorBlobLocatorProcessor.Blob blobClosestToCenter() {
+        double closest = 100000000;
+        ColorBlobLocatorProcessor.Blob returnBlob = null;
+
+        List<ColorBlobLocatorProcessor.Blob> blobs = colorLocator.getBlobs();
+        for (ColorBlobLocatorProcessor.Blob blob : blobs) {
+            double distance = getDistanceFromCenter(blob.getBoxFit());
+            if ((blob.getContourArea() > 50) && (distance < closest)) {
+                closest = distance;
+                returnBlob = blob;
+            }
+        }
+        return returnBlob;
+    }
+
+    private double getDistanceFromCenter(RotatedRect boxFit) {
+        return Math.abs(boxFit.center.x - CENTER_X) + Math.abs(boxFit.center.y - CENTER_Y);
     }
 
     @Override
     public List<QQTest> getTests() {
         return Arrays.asList(
                 new TestServo("claw_movement", CLAW_OPEN_POSITION, CLAW_CLOSE_POSITION, clawServo),
-                new TestTwoServos("wrist_movement", WRIST_START_POSITION, WRIST_TRANSFER_POSITION, leftWristServo,rightWristServo)
+                new TestTwoServos("wrist_movement", WRIST_START_POSITION, WRIST_TRANSFER_POSITION, leftWristServo,rightWristServo),
+                new TestWebcam("Webcam", webcamName)
         );
     }
     public void wristTransfer(){
@@ -90,6 +143,8 @@ public class IntakeClaw extends QQMechanism {
     public void update(Telemetry telemetry){
         rightWristServo.setPosition(rightWristServoPos);
         leftWristServo.setPosition(leftWristServoPos);
+        ColorBlobLocatorProcessor.Blob blob = blobClosestToCenter();
+        telemetry.addData("Blob angle", blob.getBoxFit().angle);
     }
 
 }
