f93411e290780a4be661a7e18b16023241cda286
carrotman189 <shreyas.thota.1@gmail.com>
2025-01-22 12:47:25 -0500

    Created a new function called setWristAngle that changes the wrists angle.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/IntakeClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/IntakeClaw.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 
+import android.util.Size;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -9,6 +11,17 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestServo;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestTwoServos;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestServo;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestWebcam;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.RotatedRect;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,31 +43,90 @@ public class IntakeClaw extends QQMechanism {
     double OPEN_TIME = 0.5;
     double leftWristServoPos;
     double rightWristServoPos;
+    WebcamName webcamName;
+    public Telemetry telemetry;
+
+    public static final int CAMERA_WIDTH = 640;
+    public static final int CAMERA_HEIGHT = 480;
+    int CENTER_X = CAMERA_WIDTH / 2;
+    int CENTER_Y = CAMERA_HEIGHT / 2;
+
+    ColorBlobLocatorProcessor colorLocator;
 
 
     @Override
     public void init(HardwareMap hwMap) {
         clawServo = hwMap.get(Servo.class, "intake_claw_servo");
         leftWristServo = hwMap.get(Servo.class, "left_intake_wrist");
-        rightWristServo = hwMap.get(Servo.class,"right_intake_wrist");
+        rightWristServo = hwMap.get(Servo.class, "right_intake_wrist");
         leftWristServo.setDirection(Servo.Direction.REVERSE);
+        webcamName = hwMap.get(WebcamName.class, "Webcam 1");
+
+        colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.RED)         // use a predefined color match
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-1, 1, 1, -1))  // search central 1/4 of camera view
+                .setDrawContours(true)                        // Show contours on the Stream Preview
+                .setBlurSize(5)                               // Smooth the transitions between different colors in image
+                .build();
+        @SuppressWarnings("unused")
+        VisionPortal portal = new VisionPortal.Builder()
+                .addProcessor(colorLocator)
+                .setCameraResolution(new Size(CAMERA_WIDTH, CAMERA_HEIGHT))
+                .setCamera(webcamName)
+                .build();
     }
 
     @Override
     public List<QQTest> getTests() {
         return Arrays.asList(
                 new TestServo("claw_movement", CLAW_OPEN_POSITION, CLAW_CLOSE_POSITION, clawServo),
-                new TestTwoServos("wrist_movement", WRIST_START_POSITION, WRIST_TRANSFER_POSITION, leftWristServo,rightWristServo)
+                new TestTwoServos("wrist_movement", WRIST_START_POSITION, WRIST_TRANSFER_POSITION, leftWristServo, rightWristServo)
         );
     }
-    public void wristTransfer(){
+    @Override
+    public void update(Telemetry telemetry){
+        rightWristServo.setPosition(rightWristServoPos);
+        leftWristServo.setPosition(leftWristServoPos);
+        ColorBlobLocatorProcessor.Blob blob = blobClosestToCenter();
+        if (blob != null) {
+            double angle = blob.getBoxFit().angle;
+            if (blob.getBoxFit().size.height > blob.getBoxFit().size.width) {
+                angle = 90 + angle;
+            }
+            setWristAngle(angle);
+        }
+    }
+    public ColorBlobLocatorProcessor.Blob blobClosestToCenter() {
+        double closest = 100000000;
+        ColorBlobLocatorProcessor.Blob returnBlob = null;
+
+        List<ColorBlobLocatorProcessor.Blob> blobs = colorLocator.getBlobs();
+        for (ColorBlobLocatorProcessor.Blob blob : blobs) {
+            double distance = getDistanceFromCenter(blob.getBoxFit());
+            if ((blob.getContourArea() > 50) && (distance < closest)) {
+                closest = distance;
+                returnBlob = blob;
+            }
+        }
+        return returnBlob;
+    }
+    private double getDistanceFromCenter(RotatedRect boxFit) {
+        return Math.abs(boxFit.center.x - CENTER_X) + Math.abs(boxFit.center.y - CENTER_Y);
+    }
+
+
+
+    public void wristTransfer() {
         leftWristServoPos = WRIST_START_POSITION;
         rightWristServoPos = WRIST_START_POSITION;
     }
-    public void wristIntake(){
+
+    public void wristIntake() {
         leftWristServoPos = WRIST_TRANSFER_POSITION;
         rightWristServoPos = WRIST_TRANSFER_POSITION;
     }
+
     public void open() {
         openTimer.reset();
         clawServo.setPosition(CLAW_OPEN_POSITION);
@@ -65,31 +137,36 @@ public class IntakeClaw extends QQMechanism {
         clawServo.setPosition(CLAW_CLOSE_POSITION);
     }
 
-    public void adjustWrist(double manualChangeAmount){
-        if (leftWristServoPos <= MAX_SERVO_POS && rightWristServoPos <= MAX_SERVO_POS){
+    public void adjustWrist(double manualChangeAmount) {
+        if (leftWristServoPos <= MAX_SERVO_POS && rightWristServoPos <= MAX_SERVO_POS) {
             leftWristServoPos += manualChangeAmount;
             rightWristServoPos -= manualChangeAmount;
         }
-    }    public boolean isClawOpen() {
+    }
+
+    public boolean isClawOpen() {
         if ((clawServo.getPosition() == CLAW_OPEN_POSITION) &&
-                (openTimer.seconds()> OPEN_TIME))
+                (openTimer.seconds() > OPEN_TIME))
             return true;
 
         return false;
     }
+
     public boolean isClawClosed() {
         if ((clawServo.getPosition() == CLAW_CLOSE_POSITION) &&
-                (closedTimer.seconds()> CLOSED_TIME))
+                (closedTimer.seconds() > CLOSED_TIME))
             return true;
 
         return false;
     }
-
-
-    @Override
-    public void update(Telemetry telemetry){
-        rightWristServo.setPosition(rightWristServoPos);
-        leftWristServo.setPosition(leftWristServoPos);
+    public void setWristAngle(double angle){
+        double servoAngle = WRIST_START_POSITION + (WRIST_TRANSFER_POSITION - WRIST_START_POSITION) * (angle/300);
+        if(telemetry != null){
+            telemetry.addData("angle", angle);
+            telemetry.addData("ServoAngle", servoAngle);
+        }
+        leftWristServo.setPosition(1-servoAngle);
+        rightWristServo.setPosition(servoAngle);
     }
 
 }
