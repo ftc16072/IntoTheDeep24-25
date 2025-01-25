c9d2f054a6caeeff0f47ab9ad38e44c481f4ea40
diya mishra <msdiyamishra@gmail.com>
2025-01-25 18:14:27 -0500

    Adding Current draw to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/ControlHub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/ControlHub.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestGyro;
 
@@ -14,15 +16,25 @@ import java.util.List;
 
 public class ControlHub extends QQMechanism{
     IMU gyro;
+    LynxModule controlHub;
+    LynxModule expansionHub;
     @Override
     public void init(HardwareMap hwMap) {
+        controlHub = (LynxModule) hwMap.get(LynxModule.class, "Control Hub");
         gyro = hwMap.get(IMU.class,"imu");
+        expansionHub = (LynxModule) hwMap.get(LynxModule.class, "Expansion Hub 2");
 
         RevHubOrientationOnRobot orientationOnRobot =
                 new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP);
 
         gyro.initialize(new IMU.Parameters(orientationOnRobot));
+    }
+    public double getControlHubCurrent(CurrentUnit currentUnit){
+        return controlHub.getCurrent(currentUnit);
+    }
+    public double getExpansionHubCurrent(CurrentUnit currentUnit){
+        return expansionHub.getCurrent(currentUnit);
     }
 
     public double getYaw(AngleUnit angleUnit){
